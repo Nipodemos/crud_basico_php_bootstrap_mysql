c6f73926da5641e4f23a577ce8f10d5e991e5fa4
nipodemos <nipodemos13@gmail.com>
2019-09-23 01:25:25 -0300

    Criando um formulário de inserção
    estou muito feliz com tamanho ensinamento, e ainda de graça!
    
    aprendi a fazer o cadastro de produto
    usando html e bootstrap, que facilita MUITO MESMO criação de sites responsivo
    eu não sabia disso até hoje
    achei que era só classes, mas não
    eles te oferecem pedaços de códigos pra tudo que vc imaginar
    muito bom

--- index.php
+++ index.php
@@ -1,17 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Meu Primeiro Script</title>
-    <script defer src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <title>Meu Primeiro Script</title>
+
+  <style type="text/css">
+    #tamanhoContainer {
+      width: 500px;
+    }
+  </style>
+
+
+  <script defer src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    <button type="button" class="btn btn-primary">Primary</button>
+  <div class="container" id="tamanhoContainer" style="margin-top:40px;">
+    <form action="">
+
+      <div class="form-group">
+        <label>Nº Produto</label>
+        <input type="number" class="form-control" placeholder="Insira o Nº do produto">
+
+      </div>
+      <div class="form-group">
+        <label>Nome Produto</label>
+        <input type="text" class="form-control" placeholder="Insira o Nome do produto">
+
+      </div>
+      <div class="form-group">
+        <label>Quantidade do Produto</label>
+        <input type="number" class="form-control" placeholder="Insira a Quantidade do produto">
+
+      </div>
+
+      <div class="form-group">
+        <label>Categoria</label>
+        <select class="form-control">
+          <option>Periféricos</option>
+          <option>Hardware</option>
+          <option>Software</option>
+          <option>Celulares</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label>Fornecedor</label>
+        <select class="form-control">
+          <option>Fornecedor A</option>
+          <option>Fornecedor B</option>
+          <option>Fornecedor C</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-success btn-sm">Cadastrar</button>
+    </form>
+  </div>
+  <!--
+  <button type="button" class="btn btn-primary">Primary</button>
     <button type="button" class="btn btn-secondary">Secondary</button>
     <button type="button" class="btn btn-success">Success</button>
     <button type="button" class="btn btn-danger">Danger</button>
@@ -21,5 +71,8 @@
     <button type="button" class="btn btn-dark">Dark</button>
 
     <button type="button" class="btn btn-link">Link</button>
+-->
+
 </body>
+
 </html>
