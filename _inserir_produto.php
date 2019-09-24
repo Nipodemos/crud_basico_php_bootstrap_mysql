dde669734c0e54dc051f0ca66b92737931bd2a11
Nipodemos <nipodemos13@gmail.com>
2019-09-24 20:27:43 -0300

    deixando o código do bootstrap num arquivo separado
    desse jeito, sempre que eu for usar  o código, é só dar um include
    em vez de ficar copiando e colando por tudo que é lugar\

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -14,9 +14,9 @@ $sql = "INSERT INTO `estoque`(`num_produto`, `nome_produto`, `categoria`, `quant
 
 $query = mysqli_query($conexao, $sql);
 
+require 'codigo_bootstrap.html';
 ?>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 
 <div class="container" style="width: 500px; margin-top: 20px;">
