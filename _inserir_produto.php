c294b1207da02920429dc1bdbc479e677c8cd343
Nipodemos <nipodemos13@gmail.com>
2019-09-24 21:49:58 -0300

    Listando os valores na tabela direto do banco de dados
    ah agora chegou a parte que eu gosto
    
    estamos listando os produtos programaticamente!
    uma coisa linda de se ver, sem repetição de código
    tudo funcionando muito bem
    
    tudo é mostrado numa tabela, organizada do jeito que eu desejar
    
    também coloquei um link pro listar produto após ter cadastrado
    só porque eu posso heheh

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -24,5 +24,6 @@ require 'codigo_bootstrap.html';
         <h4>Produto Adicionado com sucesso!</h4>
         <div style="padding-top: 20px;"></div>
         <a href="index.php" role="button" class="btn btn-sm btn-primary">Cadastrar novo item</a>
+        <a href="listar_produtos.php" role="button" class="btn btn-sm btn-primary">Listar Produtos</a>
     </center>
 </div>
--- listar_produtos.php
+++ listar_produtos.php
@@ -22,9 +22,28 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td></td>
-                </tr>
+                <?php
+                        $conexao = include 'conexao.php';
+                        $sql = "SELECT * FROM `estoque`";
+                        $busca = mysqli_query($conexao, $sql);
+                        
+                        while ($array = mysqli_fetch_array($busca)) {
+                            $id_estoque   = $array['id_produto'];
+                            $num_produto  = $array['num_produto'];
+                            $nome_produto = $array['nome_produto'];
+                            $categoria    = $array['categoria'];
+                            $quantidade   = $array['quantidade'];
+                            $fornecedor   = $array['fornecedor'];
+                            
+                            ?>
+                            <tr>
+                                <td><?php echo $num_produto  ?></td>
+                                <td><?php echo $nome_produto ?></td>
+                                <td><?php echo $categoria    ?></td>
+                                <td><?php echo $quantidade   ?></td>
+                                <td><?php echo $fornecedor   ?></td>
+                            </tr>
+                        <?php } ?>
             </tbody>
         </table>
     </div>
