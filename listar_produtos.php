a931b23b11ade3264c93d5b9047b3ef5494e9d41
Nipodemos <nipodemos13@gmail.com>
2019-09-24 21:09:46 -0300

    Preparando nossa tabela para receber valores
    Agora vai ser criado uma tela pra listar os produtos facilmente
    deixando elas bem organizadas para o dono

---
+++ listar_produtos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Listar Produtos</title>
+    <?php include 'codigo_bootstrap.html'; ?>
+</head>
+
+<body>
+    <div class="container" style="margin-top: 40px;">
+        <br>
+        <h3>Lista de Produtos</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Núm Produto</th>
+                    <th scope="col">Nome Produto</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Quantidade</th>
+                    <th scope="col">Fornecedor</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
