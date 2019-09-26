cfbcd567318a8d83eeac8cd53778660b788c4e99
Nipodemos <nipodemos13@gmail.com>
2019-09-25 21:28:03 -0300

    inserindo botão de editar e como configurar seus aspectos
    nessa aula aprendi sobre o fontAwesome
    que apesar do nome, é um site de ícones, um dos mais completos que se pode encontrar
    gratuitamente, isso se não for o maior
    além disso é muito simples de
    começar a usar no seu site
    
    colquei um botão de editar nos produtos, usando o bootstrap e o fontawesome
    
    criei o arquivo editar_produto.php que vai lidar com tudo de edição

--- listar_produtos.php
+++ listar_produtos.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Listar Produtos</title>
     <?php include 'codigo_bootstrap.html'; ?>
+    <script defer src="https://kit.fontawesome.com/9060385a42.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -19,31 +20,46 @@
                     <th scope="col">Categoria</th>
                     <th scope="col">Quantidade</th>
                     <th scope="col">Fornecedor</th>
+                    <th scope="col">Ação</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                        $conexao = include 'conexao.php';
-                        $sql = "SELECT * FROM `estoque`";
-                        $busca = mysqli_query($conexao, $sql);
-                        
-                        while ($array = mysqli_fetch_array($busca)) {
-                            $id_estoque   = $array['id_produto'];
-                            $num_produto  = $array['num_produto'];
-                            $nome_produto = $array['nome_produto'];
-                            $categoria    = $array['categoria'];
-                            $quantidade   = $array['quantidade'];
-                            $fornecedor   = $array['fornecedor'];
-                            
-                            ?>
-                            <tr>
-                                <td><?php echo $num_produto  ?></td>
-                                <td><?php echo $nome_produto ?></td>
-                                <td><?php echo $categoria    ?></td>
-                                <td><?php echo $quantidade   ?></td>
-                                <td><?php echo $fornecedor   ?></td>
-                            </tr>
-                        <?php } ?>
+                    $conexao = include 'conexao.php';
+                    $sql = "SELECT * FROM `estoque`";
+                    $busca = mysqli_query($conexao, $sql);
+                    
+                    while ($array = mysqli_fetch_array($busca)) {
+                        $id_estoque   = $array['id_produto'];
+                        $num_produto  = $array['num_produto'];
+                        $nome_produto = $array['nome_produto'];
+                        $categoria    = $array['categoria'];
+                        $quantidade   = $array['quantidade'];
+                        $fornecedor   = $array['fornecedor']; ?>
+                <tr>
+                    <td>
+                        <?php echo $num_produto  ?>
+                    </td>
+                    <td>
+                        <?php echo $nome_produto ?>
+                    </td>
+                    <td>
+                        <?php echo $categoria    ?>
+                    </td>
+                    <td>
+                        <?php echo $quantidade   ?>
+                    </td>
+                    <td>
+                        <?php echo $fornecedor   ?>
+                    </td>
+                    <td>
+                        <a class="btn btn-warning btn-sm" style="color: black;"
+                            href="editar_produto.php?id=<?php echo $id_estoque?>"
+                            role="button"><i class="fas fa-edit"></i>&nbsp;Editar</a>
+                    </td>
+                </tr>
+                <?php
+                    } ?>
             </tbody>
         </table>
     </div>
