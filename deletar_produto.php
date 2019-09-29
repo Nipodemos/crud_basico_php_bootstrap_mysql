d619b5cbf52c0b94b91e0192fecf8a4000f24e7e
nipodemos <nipodemos13@gmail.com>
2019-09-29 14:59:28 -0300

    Inserindo botão deletar em nossa tabela e deletando produtos
    aprendi a usar o comando delete do sql
    e colocar o botão de deletar na listagem de produtos
    
    acabou que foi a parte mais fácil, porque foi basicamente copiar e colar
    de outros arquivos do CRUD

---
+++ deletar_produto.php
@@ -0,0 +1,20 @@
+<?php
+
+$conexao = include 'conexao.php';
+$id = $_GET['id'];
+
+$sql = "DELETE FROM produtos WHERE id_produto=$id";
+$deletar = mysqli_query($conexao, $sql);
+
+include 'codigo_bootstrap.html';
+?>
+
+<div class="container" style="width: 400px;">
+    <center>
+        <h3>Deletado com sucesso</h3>
+        <div style="margin-top: 10px;"></div>
+        <a href="index.php" role="button" class="btn btn-sm btn-primary">Cadastrar novo item</a>
+        <a href="listar_produtos.php" role="button" class="btn btn-sm btn-primary" style="color: #fff;">Listar
+            Produtos</a>
+    </center>
+</div>
