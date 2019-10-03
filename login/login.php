89614f383167f2736e5cb58527108cefc01cbbfb
nipodemos <nipodemos13@gmail.com>
2019-10-02 22:27:07 -0300

    Seção 8: Criando a Tela de login
    primeiro foi só a parte do design da tela
    seguimos princípios que já foram ensinados
    mas dessa vez em vez do font awesome, pegamos do google mesmo
    a imagem do cadeado
    provavelmente porque isso não é um ícone
    é uma imagem, que poderia ser sua logo
    
    aprendi que tem o site https://www.cssmatic.com/box-shadow
    que vc pode criar visualmente
    a sombra que vc coloca em divs
    mais fácil ir alterando a intensidade, cor e tudo mais
    muito, mas muito interessante mesmo

---
+++ login/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <?php include '../codigo_bootstrap.html'; ?>
+    <style>
+        #tamanho {
+            width: 350px;
+            -webkit-box-shadow: 10px 10px 18px 0px rgba(140, 135, 140, 1);
+            -moz-box-shadow: 10px 10px 18px 0px rgba(140, 135, 140, 1);
+            box-shadow: 10px 10px 18px 0px rgba(140, 135, 140, 1);
+        }
+    </style>
+</head>
+
+<body>
+    <div id="tamanho" class="container" style="margin-top: 100px; border-radius: 15px; border: 2px solid #f3f3f3;">
+        <form style="padding:10px;" action="post">
+            <div class="form-group">
+                <center><img src="../imagens/cadeado.png" width="200px"></center>
+                <label>Usuário</label>
+                <input type="text" name="usuario" class="form-control" placeholder="Usuário" required>
+                <label>Senha</label>
+                <input type="password" name="usuario" class="form-control" placeholder="Senha" autocomplete="off" required>
+                <div style="text-align:right">
+                    <button style="margin-top: 15px;" type="submit" class="btn btn-success btn-sm">Entrar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
